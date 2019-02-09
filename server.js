975509068da6db9fa1d63e72b904b64dfa701e58
EmersonBrSouza <emerson_souza_fsa@hotmail.com>
2019-02-09 11:59:22 -0300

    Bye bye Express, ou será que devo chamar de Ex-press?

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
-const routes = require('./config/routes');
+'use strict'
 
+/*
+|--------------------------------------------------------------------------
+| Http server
+|--------------------------------------------------------------------------
+|
+| This file bootstrap Adonisjs to start the HTTP server. You are free to
+| customize the process of booting the http server.
+|
+| """ Loading ace commands """
+|     At times you may want to load ace commands when starting the HTTP server.
+|     Same can be done by chaining `loadCommands()` method after
+|
+| """ Preloading files """
+|     Also you can preload files by calling `preLoad('path/to/file')` method.
+|     Make sure to pass relative path from the project root.
+*/
 
-const app = express();
-app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json());
+const { Ignitor } = require('@adonisjs/ignitor')
 
-app.get('/', function (req, res) {
-  res.send({ success: "Mas é claro que sim!"})
-})
-
-
-const server = http.createServer(app);
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+new Ignitor(require('@adonisjs/fold'))
+  .appRoot(__dirname)
+  .fireHttpServer()
+  .catch(console.error)
