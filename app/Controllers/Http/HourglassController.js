35bad6ec37148f3b672a1deef52040e47ad65a34
João Sena <joaopaulo761@gmail.com>
2019-02-09 17:36:56 -0300

    Progress

---
+++ app/Controllers/Http/HourglassController.js
@@ -0,0 +1,58 @@
+'use strict'
+const Discipline = use('App/Models/Discipline')
+const ClassStat = use('App/Models/ClassStat')
+const GradeStat = use('App/Models/GradeStat')
+const crypto = require("crypto")
+
+class HourglassController {
+  async onReceiveStats ({ request }) {
+    const data = request.all()
+    const {
+      disciplines,
+      semester,
+      score,
+      userId
+    } = data
+
+    const userHash = crypto.createHash('md5').update(userId.toLowerCase()).digest('hex')
+
+    for (let i = 0; i < disciplines.length; i++) {
+      const data = disciplines[i]
+      const {
+        code,
+        disciplineName,
+        grade,
+        partialScore,
+        semester,
+        semesterName,
+        teacher
+      } = data
+
+      const teacherHash = crypto.createHash('md5').update(teacher.toLowerCase()).digest('hex')
+
+      // Creates the discipline if it doesn't exists
+      const discipline = await Discipline.findOrCreate(
+        { code },
+        { code, name: disciplineName }
+      )
+
+      // The union of a discipline a semester and a teacher generates a class
+      const clazz = await ClassStat.findOrCreate(
+        { discipline: discipline['_id'], semester, teacherHash },
+        { discipline: discipline['_id'], semester, teacher, teacherHash, semesterName }
+      )
+
+
+      const gradeStat = await GradeStat.findBy('userId', userHash) || new GradeStat()
+      gradeStat['userHash'] = userHash
+      gradeStat['class'] = clazz['_id']
+      gradeStat['grade'] = grade
+      gradeStat['partialScore'] = partialScore
+      await gradeStat.save()
+    }
+
+    return { success: true, message: 'All the elements are now in place' }
+  }
+}
+
+module.exports = HourglassController
--- start/routes.js
+++ start/routes.js
@@ -16,9 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+
 Route.on('/').render('welcome')
 
-Route.post('/save_data', async (request) => {
-  console.log(request.request.all());
-  return { success: true, message: 'completed' }
-});
+Route.post('/save_data', 'HourglassController.onReceiveStats');
